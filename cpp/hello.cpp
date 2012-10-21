9f9adffd467b11c593972049651ce08a01f0be21
Naoki Mizuno <nm2372@gmail.com>
2012-10-21 03:27:20 -0400

    starting to get insterested in C++

---
+++ cpp/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const* argv[])
+{
+    cout << "Hello World\n";
+    String name;
+    cin >> name;
+    cout << name;
+
+    return 0;
+}
