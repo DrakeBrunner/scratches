6608d239a2ef5d68f9119a8d0adbdfa245d63b2d
Naoki Mizuno <nm2372@gmail.com>
2013-03-01 22:19:19 -0500

    working version of hello world

--- android/helloworld/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/naoki/src/scratches/android/helloworld/bin/AndroidManifest.xml : \
-/home/naoki/src/scratches/android/helloworld/AndroidManifest.xml \
--- android/helloworld/bin/MyFirstApp-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/naoki/src/scratches/android/helloworld/bin/MyFirstApp-debug-unaligned.apk : \
-/home/naoki/src/scratches/android/helloworld/bin/MyFirstApp.ap_ \
-/home/naoki/src/scratches/android/helloworld/bin/classes.dex \
