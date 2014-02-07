b8a0144175c9248d7f17d08d345644ee33b8ddd3
Naoki Mizuno <nm2372@gmail.com>
2014-02-06 20:31:44 -0500

    Add C-equivalent code for input_array.asm

---
+++ assembly/input_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const* argv[]) {
+    int * a;
+    int n;
+    // Get length of array
+    scanf("%d", &n);
+    a = malloc(sizeof(int) * n);
+
+    int i;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    return 0;
+}
