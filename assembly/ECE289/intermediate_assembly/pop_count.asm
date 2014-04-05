10a1751bc2bd7b52c364651eaaa3b243949ba6f1
Naoki Mizuno <nm2372@gmail.com>
2014-04-05 17:57:38 -0400

    Add program that counts the number of 1's in a binary representation

---
+++ assembly/ECE289/intermediate_assembly/pop_count.asm
@@ -0,0 +1,43 @@
+# counts the number of 1's in it's binary representation
+
+# $s0 => the number
+# $s1 => total number of 1
+
+j main
+
+# $t0 => return address
+# $t1 => loop variable
+# $t2 => initially same as $s0 (so that $s0 don't get modified)
+popCnt:
+    move $t0, $ra   # save return address
+
+    li $t1, 0       # init loop variable
+    li $s1, 0       # set total count to 0
+
+    move $t2, $s0   # don't modify $s0
+
+.L1:
+    bge $t1, 32, exit
+
+    # AND it with 1
+    andi $t3, $t2, 1
+    add $s1, $s1, $t3
+
+    srl $t2, $t2, 1 # shift right logically (insert 0 to right)
+    #div $t2, $t2, 2
+
+    addi $t1, $t1, 1
+    j .L1
+
+exit:
+    jr $t0
+# end loop
+
+main:
+    li $s0, 5
+
+    jal popCnt
+
+    # exit
+    li $v0, 10
+    syscall
---
+++ assembly/ECE289/intermediate_assembly/pop_count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// returns the number of 1's in a word in it's binary representation
+unsigned int popCnt(unsigned int input) { 
+	unsigned int sum = 0; 
+
+    int i;
+	for (i = 0; i < 32; i++) {
+		sum += (input) & 1; 
+		input = input/2; 
+	} 
+
+	return sum; 
+}
+
+int main(int argc, char const* argv[]) {
+    int num = 5;
+    printf("%d\n", popCnt(num));
+    return 0;
+}
