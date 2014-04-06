4b2d14b15c25b28679739f24b3a978da53d2e487
Naoki Mizuno <nm2372@gmail.com>
2014-04-05 20:29:53 -0400

    Convert tabs to spaces

--- assembly/ECE289/intermediate_assembly/pop_count.c
+++ assembly/ECE289/intermediate_assembly/pop_count.c
@@ -2,15 +2,15 @@
 
 // returns the number of 1's in a word in it's binary representation
 unsigned int popCnt(unsigned int input) { 
-	unsigned int sum = 0; 
+    unsigned int sum = 0; 
 
     int i;
-	for (i = 0; i < 32; i++) {
-		sum += (input) & 1; 
-		input = input/2; 
-	} 
+    for (i = 0; i < 32; i++) {
+        sum += (input) & 1; 
+        input = input/2; 
+    } 
 
-	return sum; 
+    return sum; 
 }
 
 int main(int argc, char const* argv[]) {
