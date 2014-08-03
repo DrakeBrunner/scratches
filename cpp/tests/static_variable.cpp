9b0ed4376a10a826f105b1f6a113235a4c47551b
Naoki Mizuno <nigorojr@gmail.com>
2014-08-03 05:33:06 -0400

    Use multiple instances

--- cpp/tests/static_variable.cpp
+++ cpp/tests/static_variable.cpp
@@ -18,15 +18,28 @@ void increment(Foo f) {
 }
 
 int main() {
-    Foo f;
+    Foo f, b;
 
-    std::cout << f.i << std::endl;  // 0
-    std::cout << f.j << std::endl;  // 0
+    std::cout << "f.i    = " << f.i << std::endl;    // 0
+                                                     // Or better yet
+    std::cout << "Foo::i = " << Foo::i << std::endl; // 0
+    std::cout << "f.j    = " << f.j << std::endl;    // 0
 
-    increment(f);                   // Pass by value
+    std::cout << "Incrementing instance f" << std::endl;
+    increment(f);                                    // Pass by value
 
-    std::cout << f.i << std::endl;  // 1
-    std::cout << f.j << std::endl;  // 0
+    std::cout << "f.i    = " << f.i << std::endl;    // 1 ( =  = Foo::i)
+    std::cout << "f.j    = "<< f.j << std::endl;     // 0
+
+                                                     // Modify b
+    std::cout << "b.i    = " << b.i << std::endl;    // 1 ( =  = Foo::i)
+    std::cout << "b.j    = " << b.j << std::endl;    // 0
+
+    std::cout << "Incrementing instance b" << std::endl;
+    increment(b);                                    // Pass by value
+
+    std::cout << "b.i    = " << b.i << std::endl;    // 2 ( =  = Foo::i)
+    std::cout << "b.j    = " << b.j << std::endl;    // 0
 
     return 0;
 }
