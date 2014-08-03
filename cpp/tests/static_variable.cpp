648daf3842b95978c3722b2c7e6086863d0283de
Naoki Mizuno <nigorojr@gmail.com>
2014-08-03 05:37:41 -0400

    Fix incorrect formatting

--- cpp/tests/static_variable.cpp
+++ cpp/tests/static_variable.cpp
@@ -21,24 +21,24 @@ int main() {
     Foo f, b;
 
     std::cout << "f.i    = " << f.i << std::endl;    // 0
-                                                     // Or better yet
+    // Or better yet
     std::cout << "Foo::i = " << Foo::i << std::endl; // 0
     std::cout << "f.j    = " << f.j << std::endl;    // 0
 
     std::cout << "Incrementing instance f" << std::endl;
     increment(f);                                    // Pass by value
 
-    std::cout << "f.i    = " << f.i << std::endl;    // 1 ( =  = Foo::i)
-    std::cout << "f.j    = "<< f.j << std::endl;     // 0
+    std::cout << "f.i    = " << f.i << std::endl;    // 1 (== Foo::i)
+    std::cout << "f.j    = " << f.j << std::endl;    // 0
 
-                                                     // Modify b
-    std::cout << "b.i    = " << b.i << std::endl;    // 1 ( =  = Foo::i)
+    // Modify b
+    std::cout << "b.i    = " << b.i << std::endl;    // 1 (== Foo::i)
     std::cout << "b.j    = " << b.j << std::endl;    // 0
 
     std::cout << "Incrementing instance b" << std::endl;
     increment(b);                                    // Pass by value
 
-    std::cout << "b.i    = " << b.i << std::endl;    // 2 ( =  = Foo::i)
+    std::cout << "b.i    = " << b.i << std::endl;    // 2 (== Foo::i)
     std::cout << "b.j    = " << b.j << std::endl;    // 0
 
     return 0;
