82c71040391c3438786445993613e81c5ab8761d
Naoki Mizuno <nigorojr@gmail.com>
2014-08-03 05:20:00 -0400

    Modify code to better illustrate the difference

--- cpp/tests/static_variable.cpp
+++ cpp/tests/static_variable.cpp
@@ -3,18 +3,30 @@
 class Foo {
 public:
     static int i;
+    int j;
+
+    Foo() {
+        j = 0;
+    }
 };
 
 int Foo::i = 0;
+
+void increment(Foo f) {
+    f.i++;
+    f.j++;
+}
 
 int main() {
     Foo f;
 
     std::cout << f.i << std::endl;  // 0
+    std::cout << f.j << std::endl;  // 0
 
-    f.i++;
+    increment(f);                   // Pass by value
 
     std::cout << f.i << std::endl;  // 1
+    std::cout << f.j << std::endl;  // 0
 
     return 0;
 }
